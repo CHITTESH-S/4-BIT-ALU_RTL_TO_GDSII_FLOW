808061b7a4971b61753cdd2d4f687c09322c6b13
CHITTESH S <159535320+CHITTESH-S@users.noreply.github.com>
2025-08-12 13:41:45 +0530

    config.tcl

---
+++ openlane/config.tcl
@@ -0,0 +1,7 @@
+set ::env(DESIGN_NAME) alu
+set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/src/alu.v"
+set ::env(CLOCK_PORT) clk
+set ::env(CLOCK_PERIOD) 10.0
+set ::env(FP_CORE_UTIL) 30
+set ::env(PL_TARGET_DENSITY) 0.35
+set ::env(DIE_AREA) "0 0 100 100"
